0971c22fe084e2267d30bd850f38007095beb512
fingertippower <939408507@qq.com>
2017-04-17 15:53:22 +0800

    sophomoreWebcourseBigwork

---
+++ bigwork/js/move1.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var btn = document.getElementById('btn');
+	btn.onclick = function(){
+		window.open('download.html','','width=700,height=180,top=200,status=no,menubar=no,location=no');
+	}
+}
